54f3c9ea12c56894f0cb71a9d24b00c897c71a5d
mb010 <micah.bowles@postgrad.manchestser.ac.uk>
2023-05-20 07:47:59 +0100

    Removing copying

--- train.sh
+++ train.sh
@@ -9,17 +9,6 @@
 #SBATCH --job-name=F_MAE
 #SBATCH --output=logs/%j.%x.out
 
-# Move data to scratch for faster loading
-if [ -d /state/partition1/fmae_data ]; then
-    echo "/state/partition1/fmae_data already exists."
-else
-    echo "Copying data to /state/partition1/fmae_data"
-    mkdir /state/partition1/fmae_data
-    cp -r /share/nas2_5/mbowles/_data/FIRST /state/partition1/fmae_data/
-    cp -r /share/nas2_5/mbowles/_data/MiraBest_FITS /state/partition1/fmae_data/
-    cp -r /share/nas2_5/mbowles/_data/rgz_fits /state/partition1/fmae_data/
-fi
-
 # Source venv and call training
 source /share/nas2_5/mbowles/venv/bin/activate
 python /share/nas2_5/mbowles/mae/mae/train_timm.py
